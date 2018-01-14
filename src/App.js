ce31f59b8195cc25c72c98f4301dc0a6acab3049
rambabugovind <rambabugovind@hotmail.com>
2018-01-14 13:17:59 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome</h1>
         </header>
+        <ul class="nav masthead-nav">
+    								<li class="active"><a href="#"><i class="fa fa-home fa-2x"></i></a></li>
+    								<li><a href="https://github.com/rambabugovind"><i class="fa fa-github-square fa-2x"></i></a></li>
+    								<li><a href="https://linkedin.com/in/kawixiao"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+    								<li><a href="https://twitter.com/"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+    								<li><a href="https://facebook.com/"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+    		</ul>
         <p className="App-intro">
           Website is under construction!!
         </p>
